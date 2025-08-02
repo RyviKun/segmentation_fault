69f8d7b6c602253f407b414cc4f814b92fcf3d67
riojr100 <riojr100@gmail.com>
2025-08-02 15:35:37 +0700

    MainMenu Script

--- Assets/Script/UI/MainMenu.cs
+++ Assets/Script/UI/MainMenu.cs
@@ -5,10 +5,14 @@ using System.Collections.Generic;
 using UnityEditor;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject levelSelector;
     public void PlayGame()
     {
         Debug.Log("its been clicked");
-        SceneManager.LoadSceneAsync(1);
+        // SceneManager.LoadSceneAsync(1);
+        mainMenu.SetActive(false);
+        levelSelector.SetActive(true);
     }
 
     public void QuitGame()
